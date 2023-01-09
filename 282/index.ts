b75168579b5a9cb766dcf3a87e9bc81f9119cc7f
Henri Michel <henri@tymate.com>
2023-01-09 09:03:47 +0100

    fix: fix equality check syntax

--- 282/index.ts
+++ 282/index.ts
@@ -2,4 +2,4 @@ export const sumEveryOther = (num: number): number =>
   `${num}`
     .replace(/\D/g, '')
     .split('')
-    .reduce((acc, curr, i) => acc + (i % 2 == 1 ? Number(curr) : 0), 0);
+    .reduce((acc, curr, i) => acc + (i % 2 === 1 ? Number(curr) : 0), 0);
